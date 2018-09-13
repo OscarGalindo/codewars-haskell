a8bb28e75c6ff13f9186d3394e97ab3fc54834ff
Oscar <galindero@gmail.com>
2018-09-13 19:43:44 +0200

    refactor test name

--- test/MumblingSpec.hs
+++ test/MumblingSpec.hs
@@ -15,5 +15,5 @@ spec =
     describe "ab" $ it "returns A-Bb " $ accum "ab" `shouldBe` "A-Bb"
     describe "abc" $ it "returns A-Bb-Ccc " $ accum "abc" `shouldBe` "A-Bb-Ccc"
     describe "repeat n times" $ it "should return replicated string" $ repeatAndUpperFirst 3 'a' `shouldBe` "Aaaa"
-    describe "codewars input" $ it "should work" $ accum "ZpglnRxqenU" `shouldBe` "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu"
+    describe "if char is uppercase" $ it "should convert to lower case" $ accum "ZpglnRxqenU" `shouldBe` "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu"
 
