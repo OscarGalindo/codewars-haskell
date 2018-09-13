4d2305b3a1a12d18313e5b819b4ba665f5a88212
Oscar <galindero@gmail.com>
2018-09-13 22:27:15 +0200

    red fase

--- test/CroquetClubCategorizeMemberSpec.hs
+++ test/CroquetClubCategorizeMemberSpec.hs
@@ -12,7 +12,7 @@ module CroquetClubCategorizeMemberSpec where
       describe "membership" $ do
         describe "on empty input" $ it "return empty output" $ openOrSenior [] `shouldBe` []
         describe "on (18,20) input" $ it "return single open output" $ openOrSenior [(18,20)] `shouldBe` [Open]
-        --   openOrSenior [(18,20)] `shouldBe` [Open]
+        describe "on (55,20) input" $ it "return single open output" $ openOrSenior [(55,20)] `shouldBe` [Senior]
         --   openOrSenior [(55,20)] `shouldBe` [Senior]
         --   openOrSenior [(55,7)] `shouldBe` [Open]
         --   openOrSenior [(55,7),(55,8),(54,9)] `shouldBe` [Open, Senior, Open]
